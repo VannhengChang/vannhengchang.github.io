a0d5ec9e969e3c66727d2939f3c91249cfac5705
Heng <heng@iosdev.com>
2026-06-22 17:49:01 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://vannhengchang.github.io",
-  base: "/portfolio",
   vite: {
     plugins: [tailwindcss()],
   },
